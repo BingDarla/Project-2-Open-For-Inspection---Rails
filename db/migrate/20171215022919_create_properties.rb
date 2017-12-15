6ebe62d670c0e3ad1fa32969c2dcb2dcc152182f
Damien Lowe <ashleydamienlowe@gmail.com>
2017-12-15 13:50:45 +1100

    Property migration

---
+++ db/migrate/20171215022919_create_properties.rb
@@ -0,0 +1,13 @@
+class CreateProperties < ActiveRecord::Migration[5.1]
+  def change
+    create_table :properties do |t|
+      t.text :address
+      t.integer :landsize
+      t.integer :bedrooms
+      t.float :bathrooms
+      t.boolean :parking
+      t.bigint :expectedPrice
+      t.timestamps
+    end
+  end
+end
